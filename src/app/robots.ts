532e1653e4a81f865f251044fadd2f8461506d47
ppkuk0416 <ppkuk0416@gmail.com>
2026-06-29 10:37:14 +0900

    Align robots allow list with sitemap

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,7 +7,7 @@ export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: "*",
-      allow: ["/", "/decks", "/meta", "/about", "/rules", "/privacy"],
+      allow: ["/", "/decks", "/meta", "/about", "/rules", "/privacy", "/submit"],
       disallow: ["/admin", "/api", "/auth", "/login", "/my"],
     },
     sitemap: `${siteUrl}/sitemap.xml`,
